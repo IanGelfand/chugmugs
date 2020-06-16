81b8e6ed8fb42c17ee4d0e3ab25d6a863eb5d000
Ian Gelfand <gelfandian@gmail.com>
2020-06-16 18:36:09 -0400

    Update view all user route

--- server/api/users.js
+++ server/api/users.js
@@ -3,36 +3,48 @@ const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
+  if (isAdmin) {
+    try {
+      const users = await User.findAll({
+        attributes: ['id', 'email']
+      })
+      res.json(users)
+    } catch (err) {
+      next(err)
+    }
+  } else {
+    res.send('Sorry you are not authorized to view this :(')
   }
 })
 
 router.put('/', async (req, res, next) => {
-  const userId = req.body.id
-  try {
-    await userId.update(req.body, {where: {id: userId}})
-    res.sendStatus(204)
-  } catch (error) {
-    next(error)
+  if (isAdmin) {
+    const userId = req.body.id
+    try {
+      await userId.update(req.body, {where: {id: userId}})
+      res.sendStatus(204)
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    res.send('Sorry you are not authorized to view this :(')
   }
 })
 
 router.get('/:id', async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      attributes: ['id', 'email'],
-      where: {
-        id: req.params.id
-      }
-    })
-    res.json(user)
-  } catch (error) {
-    next(error)
+  if (isAdmin) {
+    try {
+      const user = await User.findOne({
+        attributes: ['id', 'email'],
+        where: {
+          id: req.params.id
+        }
+      })
+      res.json(user)
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    res.send('Sorry you are not authorized to view this :(')
   }
 })
