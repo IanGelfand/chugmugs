b663092537d620e47ffd8a92486788482ddb442a
Ian Gelfand <gelfandian@gmail.com>
2020-06-16 14:45:30 -0400

    Update to models and association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,9 +4,6 @@ const Mug = require('./mug')
 User.belongsToMany(Mug, {through: 'order'})
 Mug.belongsToMany(User, {through: 'order'})
 
-User.hasMany(Mug)
-Mug.hasMany(User)
-
 module.exports = {
   User,
   Mug
