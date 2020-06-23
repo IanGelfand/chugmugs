6214e3b0c8a705fd2bfc8946c15e62cb7543a333
Ian Gelfand <gelfandian@gmail.com>
2020-06-23 18:39:43 -0400

    bootstrap

--- client/components/single-mug.js
+++ client/components/single-mug.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {addMugToCart} from '../store/cart'
+import {Button} from 'react-bootstrap'
 
 const SingleMug = ({addMugToCart, location}) => {
   const mug = location.state
@@ -13,17 +14,25 @@ const SingleMug = ({addMugToCart, location}) => {
       ) : (
         <div className="single-mug-page">
           <div className="single-mug">
-            <h2>{mug.title}</h2>
-            <img src={mug.imgUrl} />
-            <h4>Color: {mug.color}</h4>
-            <h4>Capacity: {mug.capacity}oz</h4>
-            <h4>Price: ${mug.price / 100}</h4>
-            <p>Details: {mug.description}</p>
+            <div className="left">
+              <h2>{mug.title}</h2>
+              <img src={mug.imgUrl} height="400" width="400" />
+            </div>
+            <div className="right">
+              <h4>Color: {mug.color}</h4>
+              <h4>Capacity: {mug.capacity}oz</h4>
+              <h4>Price: ${mug.price / 100}</h4>
+              <p>Details: {mug.description}</p>
+            </div>
           </div>
           <div>
-            <button type="button" onClick={() => addMugToCart(mug.id)}>
+            <Button
+              variant="outline-success"
+              className="addCartButton"
+              onClick={() => addMugToCart(mug.id)}
+            >
               Add To Cart
-            </button>
+            </Button>
           </div>
         </div>
       )}
