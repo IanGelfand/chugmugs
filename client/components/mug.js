cb00c275bb03b862b10dba96c82df73cf8553b4b
Vasyl Semak <vasylsmk1401@gmail.com>
2020-06-24 00:00:03 -0400

    style: added css to cart component

--- client/components/mug.js
+++ client/components/mug.js
@@ -5,9 +5,9 @@ const Mug = ({mug}) => (
   <div className="list-mug">
     <Link to={{pathname: `/mugs/${mug.id}`, state: {...mug}}}>
       <h4>{mug.title}</h4>
-      <img src={mug.imgUrl} width="200" height="200" />
+      <img src={mug.imgUrl} width="250" height="250" />
     </Link>
-    <h5>Price: ${mug.price / 100}</h5>
+    <h5>${mug.price / 100}</h5>
   </div>
 )
 
